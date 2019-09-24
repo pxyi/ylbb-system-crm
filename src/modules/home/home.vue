a583e02e2dc9b546fc4b55bf9f12c5b5f28e1808
phuhoang <phuhoang@sina.com>
2019-09-24 11:42:38 +0800

    首页新增筹备中展示

--- src/modules/home/home.vue
+++ src/modules/home/home.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>home</div>
+  <div>
+    <img src="~@A/in-preparation.png" alt="紧急筹备中..." class="in-preparation">
+    <h2 class="pre-h2">敬请期待！</h2>
+    <p class="pre-p">此功能正在紧急开发中...</p>
+  </div>
 </template>
 
 <script>
@@ -8,6 +12,21 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .in-preparation {
+    width: 330px;
+    margin: 10% auto 30px;
+  }
+  .pre-h2 {
+    font-size: 22px;
+    line-height: 1;
+    text-align: center;
+    margin-bottom: 15px;
+  }
+  .pre-p {
+    color: #666;
+    font-size: 15px;
+    line-height: 1;
+    text-align: center;
+  }
 </style>
